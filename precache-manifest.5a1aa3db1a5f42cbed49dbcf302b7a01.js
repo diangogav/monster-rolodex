0f8f1b7a0100ade81de8f878bd6be477a3b31552
Diango <diangogavidia@gmail.com>
2019-09-13 19:13:41 -0400

    Updates

---
+++ precache-manifest.5a1aa3db1a5f42cbed49dbcf302b7a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910512e7d568c3f2499e956e870c27b4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f762bb59b0fd59b512e7",
+    "url": "/monster-rolodex/static/css/main.0944f8ce.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monster-rolodex/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "f762bb59b0fd59b512e7",
+    "url": "/monster-rolodex/static/js/main.666cd153.chunk.js"
+  },
+  {
+    "revision": "eac7e1388a413f076798",
+    "url": "/monster-rolodex/static/js/runtime~main.f2a7c9f4.js"
+  }
+]);
